00acd218224d9d7b411e383925381e7271af4d86
Othy Tenk <othy.tenk@gmail.com>
2023-12-20 18:06:47 +0800

    fix: adds prod domain url

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -2,5 +2,5 @@ export const baseURL = () => {
   return;
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "http://localhost:3000";
+    : "https://nextjs-socket-io-demo";
 };
