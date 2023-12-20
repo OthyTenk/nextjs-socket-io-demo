e186026c5b7ea3bafa574c87ed97f5ac1dc5c47a
Othy Tenk <othy.tenk@gmail.com>
2023-12-20 17:58:55 +0800

    fix: fixes fitch url

--- src/libs/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-// Create a socket connection
-// const socket = io({ autoConnect: false });
-// fetch("http://localhost:3000/api/socket/server");
-const socket = io("http://localhost:3000/api/socket/server");
-
-export default socket;
