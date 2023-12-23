73eb7bc0b11a2824a6c6e92135f79784e011cf58
Othy Tenk <othy.tenk@gmail.com>
2023-12-23 09:30:33 +0800

    fix: updates ci pipeline

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -3,7 +3,7 @@ export const baseURL = () => {
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
       : // : "https://socketionextjs.netlify.app";
-        "http://ec2-3-133-127-21.us-east-2.compute.amazonaws.com";
+        "http://ec2-3-137-182-223.us-east-2.compute.amazonaws.com/";
 
   return url;
 };
